512d940cf71341def2278c478c37b4dd79368d84
Your Name <nazmul.cse.pust@gmail.com>
2021-04-25 15:09:18 +0600

    list object map() with warning

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,8 +4,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        
-        
+        tags: ['tag1', 'tag2', 'tag3']        
 
     };
    
@@ -16,8 +15,11 @@ class Counter extends Component {
             <div> 
                 <h1>Hello world</h1>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                
                 <button className='btn btn-secondary btn-sm'>Increment</button>
+            
+            <ul>
+                {this.state.tags.map(tag => <li>{tag} </li>)}
+            </ul>
             </div>
         );
     }
