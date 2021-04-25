4b5cc08531e50a9cd0676aadf4bb2ed5b4ec372f
Your Name <nazmul.cse.pust@gmail.com>
2021-04-25 14:45:00 +0600

    local style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,10 +7,10 @@ class Counter extends Component {
         
 
     };
-    styles = {
-        fontSize:10,
-        fontWeight: "bold"
-    }
+    // styles = {
+    //     fontSize:10,
+    //     fontWeight: "bold"
+    // }
 
 
     render() { 
@@ -18,7 +18,7 @@ class Counter extends Component {
         return (
             <div> 
                 <h1>Hello world</h1>
-                <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+                <span style={{fontSize:30}} className='badge badge-primary m-2'>{this.formatCount()}</span>
                 
                 <button className='btn btn-secondary btn-sm'>Increment</button>
             </div>
