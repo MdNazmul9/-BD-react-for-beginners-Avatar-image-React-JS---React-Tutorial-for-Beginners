125a5fa7fc1123803e375308b4fd2d89af974810
Your Name <nazmul.cse.pust@gmail.com>
2021-04-26 18:02:49 +0600

    Reset button works

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -13,7 +13,12 @@ class Counters extends Component {
 
     }
     handleIncrement = counter => {
-        console.log(counter);
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter)
+        counters[index]={...counter}
+        counters[index].value++;
+        this.setState({counters});
+
     };
 
     handleReset = () =>{
