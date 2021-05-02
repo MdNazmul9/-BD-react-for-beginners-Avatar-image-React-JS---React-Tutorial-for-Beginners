e77ea528a372dacd3a96d3d62746ddc4b53604ba
Your Name <nazmul.cse.pust@gmail.com>
2021-05-02 17:12:03 +0600

    Lifting the State Up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Counters from './components/counters';
-import React from 'react';
-function App() {
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    // console.log('Event Handler called', counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
   
-  return (
-    <React.Fragment> 
+  render() { 
+    return ( 
+      <React.Fragment> 
       <Navbar />
     <main className="container">
-      <Counters />
+      <Counters 
+      counters={this.state.counters}
+      onReset={this.handleReset}
+      onDelete={this.handleDelete}
+      onIncrement={this.handleIncrement}
+      
+      />
 
     </main>
     </React.Fragment>
-    
-  );
+     );
+  }
 }
-
+ 
 export default App;
