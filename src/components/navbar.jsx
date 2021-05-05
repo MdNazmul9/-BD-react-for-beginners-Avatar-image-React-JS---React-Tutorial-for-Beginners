7a597ee20fce7f862719f4af02f1757e082654fd
Your Name <nazmul.cse.pust@gmail.com>
2021-05-05 18:29:45 +0600

    Destructuring Arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
-class Navbar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
+const NavBar = ({totalCounters}) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Navbar
+            Navbar{" "}
+            {totalCounters}
           </a>
         </div>
       </nav>
-    );
-  }
+  );
 }
  
-export default Navbar;
+export default NavBar;
