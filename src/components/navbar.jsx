feed0b44ee7286ba231f5bcd64337def0df91eb8
Your Name <nazmul.cse.pust@gmail.com>
2021-05-06 14:01:46 +0600

    Destructuring Arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 const NavBar = ({totalCounters}) => {
   return (
     <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
+        {/* <div className="container-fluid"> */}
           <a className="navbar-brand" href="#">
             Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
             {totalCounters}
+            </span>
           </a>
-        </div>
+        {/* </div> */}
       </nav>
   );
 }
